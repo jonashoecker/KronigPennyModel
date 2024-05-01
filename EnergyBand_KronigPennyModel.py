fe56e8c40405b68089acfaebc44581d0b5dcebcc
jonashoecker <69045473+jonashoecker@users.noreply.github.com>
2024-05-01 17:39:05 +0200

    Add files via upload
    Second Version

--- EnergyBand_KronigPennyModel.py
+++ EnergyBand_KronigPennyModel.py
@@ -5,9 +5,14 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
-v = 10
+v = 100
 def equation(e, cos_kxi):
-    return m.cos(m.sqrt(e)) * m.cos(m.sqrt(v + e)) - ((v/2) + e)/(m.sqrt(e * (v + e))) * m.sin(m.sqrt(e)) * m.sin(m.sqrt(v + e)) - cos_kxi
+    if e > 0:
+        return m.cos(m.sqrt(e)) * m.cos(m.sqrt(v + e)) - ((v/2) + e)/(m.sqrt(e * (v + e))) * m.sin(m.sqrt(e)) * m.sin(m.sqrt(v + e)) - cos_kxi
+    else:
+        e = -e
+        return m.cosh(m.sqrt(e)) * m.cos(m.sqrt(v - e)) - ((v / 2) - e) / (m.sqrt(e * (v - e))) * m.sinh(
+            m.sqrt(e)) * m.sin(m.sqrt(v - e)) - cos_kxi
 
 def numerical_derivative(f, x, cos_kxi, h=1e-6):
     return (f(x + h, cos_kxi) - f(x, cos_kxi))/h
@@ -20,7 +25,11 @@ def newton(f, initial_guess, cos_kxi, tolerance=1e-6):
     fn = 0.
     while True:
         ic += 1
+        if x == 0:
+            return None
         fn = f(x, cos_kxi)
+        if 0 > fn and fn > 2*m.pi:
+            return None
         der_f = numerical_derivative(f, x, cos_kxi)
         new_x = x - fn / der_f
         d = m.fabs(new_x - x)
@@ -43,17 +52,29 @@ def find_e_values(guess, equation):
     kxi_values = []
     for kxi in np.arange(0., 2*m.pi, 0.001):
         cos_kxi = m.cos(kxi)
-        print('-------------------------------', kxi, cos_kxi)
+        print('-------------------------------', kxi, cos_kxi, guess)
         e_value = newton(equation, guess, cos_kxi)
+        if e_value == None:
+            return None
         e_values.append(e_value)
         kxi_values.append(kxi)
         guess = e_value
     return kxi_values, e_values
 
-guess_values = [6, 34, 84]
+guess_values = [i for i in range(-80, 120, 10)]
+previous_band = None
+number = 0
+tolerance = 2
 for index, guess in enumerate(guess_values):
-    kxi_values, e_values = find_e_values(guess, equation)
-    plt.plot(kxi_values, e_values, label=f"Energy band {index+1}")
+    result = find_e_values(guess, equation)
+    if result is not None:
+        kxi_values, e_values = result
+        current_band = np.array(e_values)
+        if previous_band is None or np.linalg.norm(current_band - previous_band) > tolerance:
+            plt.plot(kxi_values, e_values, label=f"Energy band {number+1}")
+            previous_band = current_band
+            number += 1
+
 
 plt.title("Energy bands of the Kronig Penney Model")
 plt.xlabel(r'$k\xi$')
